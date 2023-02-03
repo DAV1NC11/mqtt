63f5c8fc2786b186407f31e30ccdf6e3e9b3ec12
DAV1NC11 <chagajohn@outlook.com>
2023-02-03 12:07:27 +0300

    modified broker.py to act as client

--- Broker/broker.py
+++ Broker/broker.py
@@ -1,14 +1,34 @@
-import paho.mqtt.server as mqtt
+import paho.mqtt.client as mqtt
 
+# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code"+str(rc))
-    client.subscribe("#")
+    print("Connected with result code "+str(rc))
 
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("topic")
+
+# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
+# Create the MQTT client and set the callbacks
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
 
-server = mqtt.Server()
-server.on_connect = on_connect
-server.on_message = on_message
-server.listen(1883)
+# Connect to the MQTT broker
+client.connect("localhost", 1883, 60)
+
+# Start the network loop
+client.loop_start()
+
+# Publish a message to a topic
+client.publish("topic", "Hello, MQTT!")
+
+# Wait for some time
+import time
+time.sleep(5)
+
+# Stop the network loop
+client.loop_stop()
